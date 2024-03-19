eb3065b77bb7e29b1d4489204cb1ca5b87676a75
Etikala Tejaswi <tejaswietikala.27@gmail.com>
2024-03-19 15:36:42 +0530

    Added the code.

---
+++ src/main/java/Variables.java
@@ -0,0 +1,29 @@
+package com.qualizeal.javaapp.mainprogram;
+
+public class Variables {
+    public static void main(String[] args) {
+        byte byteVar = 120;
+        short shortVar = (short) 3e4;
+        int intVar = 150000;
+        long longVar = 12345678900L;
+
+        float floatVar = 3.14f;
+        double doubleVar = 3.141592653589793e93;
+
+        boolean booleanVar = true;
+
+        char charVar = 'A';
+
+        String stringVar = "Hello, World!";
+
+        System.out.println("Byte Variable: " + byteVar);
+        System.out.println("Short Variable: " + shortVar);
+        System.out.println("Int Variable: " + intVar);
+        System.out.println("Long Variable: " + longVar);
+        System.out.println("Float Variable: " + floatVar);
+        System.out.println("Double Variable: " + doubleVar);
+        System.out.println("Boolean Variable: " + booleanVar);
+        System.out.println("Character Variable: " + charVar);
+        System.out.println("String Variable: " + stringVar);
+    }
+}
